bad28cc6a83e96f1a6d3b42c61390f55091d0243
Kaue Miziara <kauemiziara@gmail.com>
2024-10-06 17:28:01 -0300

    style: format code in WebKit C++ style

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,60 @@
 #include "Arduino.h"
 
 class DCMotor {
-  int spd, pin1, pin2;
+    int spd, pin1, pin2;
 
 public:
-  void Pinout(int in1, int in2) {
-    pin1 = in1;
-    pin2 = in2;
-    pinMode(pin1, OUTPUT);
-    pinMode(pin2, OUTPUT);
-  }
+    void Pinout(int in1, int in2)
+    {
+        pin1 = in1;
+        pin2 = in2;
+        pinMode(pin1, OUTPUT);
+        pinMode(pin2, OUTPUT);
+    }
 
-  void Speed(int in1) { spd = in1; }
+    void Speed(int in1) { spd = in1; }
 
-  void Forward() {
-    analogWrite(pin1, spd);
-    analogWrite(pin2, 0);
-  }
+    void Forward()
+    {
+        analogWrite(pin1, spd);
+        analogWrite(pin2, 0);
+    }
 
-  void Backward() {
-    analogWrite(pin1, 0);
-    analogWrite(pin2, spd);
-  }
+    void Backward()
+    {
+        analogWrite(pin1, 0);
+        analogWrite(pin2, spd);
+    }
 
-  void Stop() {
-    digitalWrite(pin1, LOW);
-    digitalWrite(pin2, LOW);
-  }
+    void Stop()
+    {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, LOW);
+    }
 };
 
 DCMotor Motor1, Motor2;
 
-void setup() {
-  Motor1.Pinout(6, 7);
-  Motor2.Pinout(9, 10);
+void setup()
+{
+    Motor1.Pinout(6, 7);
+    Motor2.Pinout(9, 10);
 }
 
-void loop() {
-  Motor1.Speed(200);
-  Motor2.Speed(200);
+void loop()
+{
+    Motor1.Speed(200);
+    Motor2.Speed(200);
 
-  Motor1.Forward();
-  Motor2.Forward();
-  delay(1000);
+    Motor1.Forward();
+    Motor2.Forward();
+    delay(1000);
 
-  Motor1.Backward();
-  Motor2.Backward();
-  delay(1000);
+    Motor1.Backward();
+    Motor2.Backward();
+    delay(1000);
 
-  Motor1.Stop();
-  Motor2.Stop();
-  delay(250);
+    Motor1.Stop();
+    Motor2.Stop();
+    delay(250);
 }
